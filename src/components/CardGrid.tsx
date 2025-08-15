8c3fda303b9caa5ecf3985b124c37dd9a8041b1b
Beans Kuhlman <bkuhlman80@gmail.com>
2025-08-14 18:44:46 -0600

    Use SIGN_LIST for grid

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -1,11 +1,11 @@
-import { SIGNS } from "@/data/signs";
 import Card from "./Card";
+import { SIGN_LIST } from "@/data/signs";
 
-export default function CardGrid(){
+export default function CardGrid() {
   return (
     <section className="mx-auto max-w-6xl p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      {SIGNS.map(s => (
-        <Card key={s.slug} {...s}/>
+      {SIGN_LIST.map(s => (
+        <Card key={s.slug} {...s} />
       ))}
     </section>
   );
